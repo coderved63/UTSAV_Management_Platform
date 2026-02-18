891de2dcba467313cf50606883f0dbae8ebdaf3f
coderved63 <coderved63@gmail.com>
2026-02-18 14:00:48 +0530

    Organization dashboard modification

--- src/app/dashboard/create/page.tsx
+++ src/app/dashboard/create/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { useState, useEffect } from "react";
 import { createOrganizationAction, getOrganizationCountAction } from "@/actions/organization.actions";
 import { useRouter } from "next/navigation";
